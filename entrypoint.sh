087c39c22aadbfbef1a3fb7348d183dc94c2f5f8
Daniel Joudat <danieljoudat@gmail.com>
2022-05-16 00:46:11 +0300

    update  entrypoint.sh
    Signed-off-by: Daniel Joudat <danieljoudat@gmail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,19 +4,27 @@ set -e
 cd m3o-js
 WORK_DIR=$(pwd)
 
-echo "Working directory ${WORK_DIR}"
-echo 'setting up m3o-js for publishing...'
+echo "ts-publisher-action-> Working directory ${WORK_DIR}"
+echo 'ts-publisher-action-> setting up m3o-js for publishing...'
 
 ls -al
 ts-publish-setup
 
-echo 'formatting ...'
+echo 'ts-publisher-action-> formatting ...'
 prettier -w .
 
-echo 'publishing m3o-js to npm ...'
-if [! -f .npmrc ]; then echo "missing .npmrc" && exit 1; fi
+echo 'ts-publisher-action-> publishing m3o-js to npm ...'
+
+if [ ! -f .npmrc ]
+then
+        echo "missing .npmrc"
+        exit 1
+fi
+
 npm install
 npm run build
 npm publish --access public
+
+rm .npmrc
 
 # echo "::set-output name=success::$success"
